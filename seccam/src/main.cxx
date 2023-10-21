631f7430663314131f891fd76351ecec0e985837
kmt-t <t-matsu@remoteplace.net>
2023-10-21 20:10:20 +0900

    first commit.

---
+++ seccam/src/main.cxx
@@ -0,0 +1,11 @@
+/**
+ * by Akihabara Robot Community (MCU research div).
+ */
+#include <iostream>
+
+int main(int argc, const char** argv) {
+  argc = argc;
+  argv = argv;
+  std::cout << "hello world." << std::endl;
+  return 0;
+}
